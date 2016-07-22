907c3522de15785d588bf712727a01a038408887
Ville Saarinen <ville.saarinen@gmail.com>
2016-07-22 10:51:06 +0300

    Add source information

--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,7 @@ const loaders = [
       'sass-loader',
     ],
   },
-  // For Font Awesome
+  // For Font Awesome. From https://github.com/gowravshekar/font-awesome-webpack
   {
     test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
     loader: 'url-loader?limit=10000&mimetype=application/font-woff',
