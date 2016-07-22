8d684262ba938df6ddbf29beab5c429300200c22
Ville Saarinen <ville.saarinen@gmail.com>
2016-07-22 17:50:13 +0300

    Fix export error in type declaration

--- types/react-fontawesome.d.ts
+++ types/react-fontawesome.d.ts
@@ -1,24 +1,20 @@
 import * as React from 'react';
 
-declare namespace ReactFontAwesome {
-  export class ReactFontAwesome extends React.Component<ReactFontAwesomeProps, any> {}
+declare class ReactFontAwesome extends React.Component<ReactFontAwesomeProps, any> {}
 
-  export interface ReactFontAwesomeProps {
-    border?: boolean;
-    className?: string;
-    fixedWidth?: boolean;
-    flip?: 'horizontal' | 'vertical';
-    inverse?: boolean;
-    name: string;
-    pulse?: boolean;
-    rotate?: number;
-    size?: 'lg' | '2x' | '3x' | '4x' | '5x';
-    spin?: boolean;
-    stack?: '1x' | '2x';
-  }
+interface ReactFontAwesomeProps {
+  border?: boolean;
+  className?: string;
+  fixedWidth?: boolean;
+  flip?: 'horizontal' | 'vertical';
+  inverse?: boolean;
+  name: string;
+  pulse?: boolean;
+  rotate?: number;
+  size?: 'lg' | '2x' | '3x' | '4x' | '5x';
+  spin?: boolean;
+  stack?: '1x' | '2x';
 }
 
-declare var ReactFontAwesomeType: typeof ReactFontAwesome.ReactFontAwesome;
-declare module 'react-fontawesome' {
-  export = ReactFontAwesomeType;
-}
+declare var ReactFontAwesomeType: typeof ReactFontAwesome;
+export = ReactFontAwesomeType;
