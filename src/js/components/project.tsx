ef5b9845d519fc132d19a1bc6a6ee912e9465b39
Ville Saarinen <ville.saarinen@gmail.com>
2016-07-26 10:21:56 +0300

    Fix prop typings

--- src/js/components/project.tsx
+++ src/js/components/project.tsx
@@ -4,20 +4,22 @@ import { Link } from 'react-router';
 
 import projects from '../modules/projects';
 
-interface ProjectProps {
+interface PassedProps {
   id: string;
-  name?: string;
 }
 
-const Project = ({ id, name }: ProjectProps) => (
+interface GeneratedProps {
+  name: string;
+}
+
+const Project = ({ id, name }: PassedProps & GeneratedProps) => (
   <div>
     <Link to={`/project/${id}`}>{name}</Link>
   </div>
 );
 
-const mapStateToProps = (state: any, ownProps: ProjectProps) => ({
-  name: projects.selectors.getName(state, ownProps.id),
-  id: ownProps.id, // Needed when using stateless classes: https://github.com/reactjs/react-redux/issues/290
+const mapStateToProps = (state: any, ownProps: PassedProps) => ({
+  name: projects.selectors.getName(state, ownProps.id)
 });
 
-export default connect(mapStateToProps)(Project);
+export default connect<GeneratedProps, {}, PassedProps>(mapStateToProps)(Project);
