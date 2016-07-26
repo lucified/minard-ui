2658df5acf7761d6ece0c6893317edd04e5c3d2f
Ville Saarinen <ville.saarinen@gmail.com>
2016-07-26 19:36:31 +0300

    Add type to selector

--- src/js/modules/projects/selectors.ts
+++ src/js/modules/projects/selectors.ts
@@ -1,10 +1,11 @@
 import * as _ from 'lodash';
 
 import { StateTree } from '../../reducers';
+import { Project } from './types';
 
 const selectProjectTree = (state: StateTree) => state.entities.projects;
 
-export const getProjects = (state: StateTree) => _.values(selectProjectTree(state));
+export const getProjects = (state: StateTree) => _.values<Project>(selectProjectTree(state));
 export const getIDs = (state: StateTree) => Object.keys(selectProjectTree(state));
 export const getProject = (state: StateTree, id: string) => selectProjectTree(state)[id];
 export const getBranches = (state: StateTree, id: string) => selectProjectTree(state)[id].branches;
