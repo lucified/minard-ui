3ef5ca0a1d5cc5f4be32ba4da7f25d7ff69a604d
Ville Saarinen <ville.saarinen@gmail.com>
2016-07-27 11:47:57 +0300

    Realistic branch names

--- src/js/modules/branches/reducer.ts
+++ src/js/modules/branches/reducer.ts
@@ -4,14 +4,14 @@ import { BranchState } from './types';
 const initialState: BranchState = {
   1: {
     id: '1',
-    name: 'First branch',
+    name: 'first-branch',
     project: '1',
     description: 'This is a branch description',
     commits: ['1-aacceeff02', '1-12354124', '1-2543452', '1-098325343', '1-29832572fc1', '1-29752a385'],
   },
   2: {
     id: '2',
-    name: 'Second branch',
+    name: 'second-branch',
     project: '1',
     commits: ['2-aacd00f02', '2-a998823423'],
   },
