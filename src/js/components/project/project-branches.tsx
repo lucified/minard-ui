ea1f889c4a8c231a388e2697ca3edf1344a068d4
Ville Saarinen <ville.saarinen@gmail.com>
2016-07-27 11:34:10 +0300

    Get empty content from methods

--- src/js/components/project/project-branches.tsx
+++ src/js/components/project/project-branches.tsx
@@ -12,25 +12,35 @@ interface Props {
   project: Project;
 }
 
-const ProjectBranches = ({ branches, project }: Props) => (
-  <div>
-    <SectionTitle>Branches</SectionTitle>
-    {(branches.length === 0) ? (
+class ProjectBranches extends React.Component<Props, any> {
+  private getEmptyContent() {
+    return (
       <div className="empty">
         <Icon name="exclamation" fixedWidth size="3x" />
         <p className="empty-title">No branches</p>
         <p className="empty-meta">Is your repository set up correctly?</p>
       </div>
-    ) : (
-      <div className="columns">
-        <div className="column col-1" />
-        <div className="column col-10">
-          {branches.map(branch => <BranchSummary key={branch.id} branch={branch} project={project} />)}
-        </div>
-        <div className="column col-1" />
+    );
+  }
+
+  public render() {
+    const { branches, project } = this.props;
+
+    return (
+      <div>
+        <SectionTitle>Branches</SectionTitle>
+        {(branches.length === 0) ? this.getEmptyContent() : (
+          <div className="columns">
+            <div className="column col-1" />
+            <div className="column col-10">
+              {branches.map(branch => <BranchSummary key={branch.id} branch={branch} project={project} />)}
+            </div>
+            <div className="column col-1" />
+          </div>
+        )}
       </div>
-    )}
-  </div>
-);
+    );
+  }
+};
 
 export default ProjectBranches;
