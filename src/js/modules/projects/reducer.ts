c8c47649b934135d25e8a0052c431f68e1461a84
Ville Saarinen <ville.saarinen@gmail.com>
2016-07-28 17:03:29 +0300

    Change email addresses and add activeUsers to projects

--- src/js/modules/projects/reducer.ts
+++ src/js/modules/projects/reducer.ts
@@ -7,11 +7,13 @@ const initialState: ProjectState = {
     name: 'First project',
     description: 'This is the first project description. It might not be set.',
     branches: ['1', '2', '3'],
+    activeUsers: ['ville.saarinen@lucify.com', 'juho@lucify.com']
   },
   2: {
     id: '2',
     name: 'Second project',
     branches: [],
+    activeUsers: ['ville.saarinen@lucify.com']
   },
 };
 
