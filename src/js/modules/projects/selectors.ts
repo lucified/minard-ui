8a76e8d111e5948251fbed24cf2d74a746d88f2c
Ville Saarinen <ville.saarinen@gmail.com>
2016-07-28 18:55:45 +0300

    Remove unused selector

--- src/js/modules/projects/selectors.ts
+++ src/js/modules/projects/selectors.ts
@@ -6,6 +6,5 @@ import { Project } from './types';
 const selectProjectTree = (state: StateTree) => state.entities.projects;
 
 export const getProjects = (state: StateTree) => _.values<Project>(selectProjectTree(state));
-export const getIDs = (state: StateTree) => Object.keys(selectProjectTree(state));
 export const getProject = (state: StateTree, id: string) => selectProjectTree(state)[id];
 export const getBranches = (state: StateTree, id: string) => selectProjectTree(state)[id].branches;
