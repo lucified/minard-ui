9a44724d71b0d15c28a3072cfc699aff1c9ae839
Ville Saarinen <ville.saarinen@gmail.com>
2016-08-02 19:38:42 +0300

    Remove obsolete code

--- src/js/modules/branches/selectors.ts
+++ src/js/modules/branches/selectors.ts
@@ -1,16 +1,5 @@
-import { values } from 'lodash';
-import { createSelector } from 'reselect';
-
 import { StateTree } from '../../reducers';
-import { Branch } from './types';
 
 const selectBranchTree = (state: StateTree) => state.entities.branches;
 
-const getBranches = createSelector(
-  selectBranchTree,
-  branches => values<Branch>(branches)
-);
-
 export const getBranch = (state: StateTree, id: string) => selectBranchTree(state)[id];
-export const getBranchByNameAndProject = (state: StateTree, name: string, projectId: string) =>
-  _.find(getBranches(state), branch => branch.name === name && branch.project === projectId);
