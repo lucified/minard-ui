68e5a0baca3d1606fb1aed888b6f81d39deed34f
Ville Saarinen <ville.saarinen@gmail.com>
2016-08-04 17:15:45 +0300

    Make ScreenshotPile a stateless function

--- src/js/components/common/screenshot-pile.tsx
+++ src/js/components/common/screenshot-pile.tsx
@@ -12,40 +12,36 @@ interface Props {
   deployments: Deployment[];
 }
 
-class ScreenshotPile extends React.Component<Props, any> {
-  public render() {
-    const { deployments } = this.props;
-
-    if (deployments.length === 0) {
-      return (
-        <div className={classNames(styles.pile, styles.empty)}>
-          <Icon name="times" size="3x" />
-        </div>
-      );
-    }
-
-    if (some(deployments, isNil)) {
-      return (
-        <div className={classNames(styles.pile, styles.empty)}>
-           <Icon name="circle-o-notch" spin fixedWidth size="3x" />
-        </div>
-      );
-    }
-
-    const latestDeployments = deployments.sort((a, b) => a.creator.timestamp - b.creator.timestamp);
-
+const ScreenshotPile = ({ deployments }: Props) => {
+  if (deployments.length === 0) {
     return (
-      <div className={styles.pile}>
-        {latestDeployments.slice(0, 4).map((deployment, i) =>
-          <img
-            key={deployment.id}
-            className={classNames('img-responsive', styles.screenshot, styles[`screenshot-${i}`])}
-            src={screenshot /* TODO: deployment.screenshot */}
-          />
-        )}
+      <div className={classNames(styles.pile, styles.empty)}>
+        <Icon name="times" size="3x" />
       </div>
     );
   }
+
+  if (some(deployments, isNil)) {
+    return (
+      <div className={classNames(styles.pile, styles.empty)}>
+          <Icon name="circle-o-notch" spin fixedWidth size="3x" />
+      </div>
+    );
+  }
+
+  const latestDeployments = deployments.sort((a, b) => a.creator.timestamp - b.creator.timestamp);
+
+  return (
+    <div className={styles.pile}>
+      {latestDeployments.slice(0, 4).map((deployment, i) =>
+        <img
+          key={deployment.id}
+          className={classNames('img-responsive', styles.screenshot, styles[`screenshot-${i}`])}
+          src={screenshot /* TODO: deployment.screenshot */}
+        />
+      )}
+    </div>
+  );
 }
 
 export default ScreenshotPile;
