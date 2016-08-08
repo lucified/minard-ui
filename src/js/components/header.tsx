1d8e6ff193af38f55101987d87ef5ddc332af627
Ville Saarinen <ville.saarinen@gmail.com>
2016-08-08 20:39:47 +0300

    Show global errors in header

--- src/js/components/header.tsx
+++ src/js/components/header.tsx
@@ -1,28 +1,49 @@
 import * as React from 'react';
 import * as Icon from 'react-fontawesome';
+import { connect } from 'react-redux';
 import { Link } from 'react-router';
 
-interface Props {
+import Errors, { FetchError } from '../modules/errors';
+import { StateTree } from '../reducers';
+
+interface PassedProps {
 
 }
 
-class Header extends React.Component<Props, any> {
-  public render() {
-    return (
-      <div className="columns">
-        <div className="column col-12">
-          <header className="navbar nav-default">
-            <section className="navbar-header">
-              <Link to="/" className="navbar-brand">Minard</Link>
-            </section>
-            <section className="navbar-section">
-              <a href="#"><Icon name="user" size="lg" /></a>
-            </section>
-          </header>
-        </div>
-      </div>
-    );
-  }
+interface GeneratedProps {
+  errors: FetchError[];
 }
 
-export default Header;
+const getErrorToast = (error: FetchError) => (
+  <div className="toast toast-danger">
+    <button className="btn btn-clear float-right" />
+    <span className="icon icon-error_outline" />
+    {error.error}
+  </div>
+);
+
+const Header = ({ errors }: PassedProps & GeneratedProps) => (
+  <div className="columns">
+    <div className="column col-12">
+      <header className="navbar nav-default">
+        <section className="navbar-header">
+          <Link to="/" className="navbar-brand">Minard</Link>
+        </section>
+        <section className="navbar-section">
+          {errors && errors.length > 0 && getErrorToast(errors[0])}
+        </section>
+        <section className="navbar-section">
+          <a href="#"><Icon name="user" size="lg" /></a>
+        </section>
+      </header>
+    </div>
+  </div>
+);
+
+const mapStateToProps = (state: StateTree, ownProps: PassedProps): GeneratedProps => {
+  return {
+    errors: Errors.selectors.getErrors(state),
+  };
+};
+
+export default connect<GeneratedProps, {}, PassedProps>(mapStateToProps)(Header);
