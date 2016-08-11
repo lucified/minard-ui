054fda758ef3aaa9a528f29def04ea17b7de530d
Ville Saarinen <ville.saarinen@gmail.com>
2016-08-11 18:46:27 +0300

    More robust API URL generation

--- src/js/api/index.ts
+++ src/js/api/index.ts
@@ -4,7 +4,12 @@ if (!process.env.CHARLES) {
   throw new Error('API host not defined!');
 }
 
-const host: string = `${process.env.CHARLES}/api`;
+let host: string = process.env.CHARLES;
+// Remove trailing /
+if (host.slice(-1) === '/') {
+  host = host.slice(0, -1);
+}
+const path: string = `${host}/api`;
 
 function callApi(url: string) {
   return fetch(url, { credentials: 'same-origin' })
@@ -23,10 +28,10 @@ function callApi(url: string) {
     }));
 }
 
-export const fetchActivities = () => callApi(`${host}/activity`);
-export const fetchActivitiesForProject = (id: string) => callApi(`${host}/activity?filter=project[${id}]`);
-export const fetchAllProjects = () => callApi(`${host}/teams/1/projects`); // TODO: add actual team ID
-export const fetchProject = (id: string) => callApi(`${host}/projects/${id}`);
-export const fetchBranch = (id: string) => callApi(`${host}/branches/${id}`);
-export const fetchDeployment = (id: string) => callApi(`${host}/deployments/${id}`);
-export const fetchCommit = (id: string) => callApi(`${host}/commits/${id}`);
+export const fetchActivities = () => callApi(`${path}/activity`);
+export const fetchActivitiesForProject = (id: string) => callApi(`${path}/activity?filter=project[${id}]`);
+export const fetchAllProjects = () => callApi(`${path}/teams/1/projects`); // TODO: add actual team ID
+export const fetchProject = (id: string) => callApi(`${path}/projects/${id}`);
+export const fetchBranch = (id: string) => callApi(`${path}/branches/${id}`);
+export const fetchDeployment = (id: string) => callApi(`${path}/deployments/${id}`);
+export const fetchCommit = (id: string) => callApi(`${path}/commits/${id}`);
