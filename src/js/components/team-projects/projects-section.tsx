ea6ff3c743622b53245e3e24d4e6450fde1d21bd
Ville Saarinen <ville.saarinen@gmail.com>
2016-08-15 17:56:12 +0300

    Add section titles

--- src/js/components/team-projects/projects-section.tsx
+++ src/js/components/team-projects/projects-section.tsx
@@ -1,5 +1,5 @@
+import * as classNames from 'classnames';
 import * as React from 'react';
-import * as Icon from 'react-fontawesome';
 
 import { FetchError } from '../../modules/errors';
 import { Project } from '../../modules/projects';
@@ -7,31 +7,44 @@ import { Project } from '../../modules/projects';
 import SectionTitle from '../common/section-title';
 import ProjectSummary from './project-summary';
 
+const styles = require('./projects-section.scss');
+
 interface Props {
   projects?: (Project | FetchError)[];
 }
-
-const loadingBanner = (
-  <div className="empty">
-    <Icon name="circle-o-notch" spin fixedWidth size="3x" />
-    <p className="empty-title">Loading projects</p>
-    <p className="empty-meta">Hold on a sec…</p>
-  </div>
-);
 
 const projectInfo = (projects: (Project | FetchError)[]) =>
   projects.map(project => <ProjectSummary key={project.id!} project={project} />);
 
 const ProjectsSection = ({ projects }: Props) => (
   <div>
-    <SectionTitle>Projects</SectionTitle>
-    <div className="columns">
-      <div className="column col-1" />
-      <div className="column col-10">
-        { projects ? projectInfo(projects) : loadingBanner }
+    <SectionTitle
+      rightContent={(
+        <a href="#" className={classNames(styles['add-project-link'])}>
+          + Add new project
+        </a>
+      )}
+    >
+      {projects ? (
+        <span>
+          {projects.length} projects for{' '}
+          <strong>Team Lucify</strong>
+        </span>
+      ) : (
+        <span>
+          Loading projects...
+        </span>
+      )}
+    </SectionTitle>
+    {projects && (
+      <div className="columns">
+        <div className="column col-1" />
+        <div className="column col-10">
+          {projectInfo(projects)}
+        </div>
+        <div className="column col-1" />
       </div>
-      <div className="column col-1" />
-    </div>
+    )}
   </div>
 );
 
