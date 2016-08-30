4a2c03ad9e6322f6fc1caa79bedab92ad8800f7f
Ville Saarinen <ville.saarinen@gmail.com>
2016-08-30 10:50:08 +0300

    Move form into its own component
    If this is not done, the form always exists and is not cleared when we close the dialog

--- src/js/components/team-projects-view/new-project-dialog.tsx
+++ src/js/components/team-projects-view/new-project-dialog.tsx
@@ -2,12 +2,13 @@ import * as React from 'react';
 import * as ModalDialog from 'react-modal';
 import { connect } from 'react-redux';
 import { Dispatch } from 'redux';
-import { Field, BaseFieldProps, FormProps, reduxForm } from 'redux-form';
 
+import { isError } from '../../modules/errors';
 import Modal, { ModalType } from '../../modules/modal';
 import Projects, { Project } from '../../modules/projects';
-import { isError } from '../../modules/errors';
 import { StateTree } from '../../reducers';
+
+import NewProjectForm from './new-project-form';
 
 const styles = require('../common/modal-dialog.scss');
 
@@ -28,40 +29,12 @@ interface FormData {
   description?: string;
 }
 
-type Props = PassedProps & GeneratedStateProps & GeneratedDispatchProps & FormProps<FormData, any>;
-
-const validate = (values: FormData, props: Props) => {
-  const errors: FormData = {};
-  const projectNameRegex = /^[a-z0-9\-]+$/;
-
-  const { name, description } = values;
-
-  if (!name) {
-    errors.name = 'Required';
-  } else if (!projectNameRegex.test(name)) {
-    errors.name = 'Only lower-case letters, numbers and dashes allowed.';
-  } else if (props.existingProjectNames.indexOf(name) > -1) {
-    errors.name = 'Project name already exists';
-  }
-
-  return errors;
-};
-
-const RenderField = ({ input, name, label, placeholder, type, meta: { touched, error }}: BaseFieldProps) => (
-  <div className="row">
-    <div className="col-xs-4">
-      <label htmlFor={name}>{label}</label>
-    </div>
-    <div className="col-xs-8">
-      <input {...input} placeholder={placeholder} type={type} />
-      {touched && error && <span className={styles.error}>{error}</span>}
-    </div>
-  </div>
-);
+type Props = PassedProps & GeneratedStateProps & GeneratedDispatchProps;
 
 class NewProjectDialog extends React.Component<Props, any> {
   public render() {
-    const { isOpen, closeDialog, handleSubmit, pristine, submitting } = this.props;
+    const { isOpen, closeDialog, existingProjectNames } = this.props;
+
     return (
       <ModalDialog
         isOpen={isOpen}
@@ -70,15 +43,7 @@ class NewProjectDialog extends React.Component<Props, any> {
         overlayClassName={styles.overlay}
       >
         <h1>Create a new project</h1>
-        <form onSubmit={handleSubmit}>
-          <Field name="name" component={RenderField} type="text" label="Name" placeholder="my-project-name" />
-          <Field name="description" component={RenderField} type="textarea" label="Description" placeholder="Describe your project" />
-          <div className="row">
-            <div className="col-xs-12">
-              <button type="submit" disabled={pristine || submitting}>Create project</button>
-            </div>
-          </div>
-        </form>
+        <NewProjectForm existingProjectNames={existingProjectNames} />
       </ModalDialog>
     );
   }
@@ -97,9 +62,4 @@ const mapDispatchToProps = (dispatch: Dispatch<any>) => ({
 export default connect<GeneratedStateProps, GeneratedDispatchProps, PassedProps>(
   mapStateToProps,
   mapDispatchToProps,
-)(
-  reduxForm({
-    form: 'newProject',  // a unique identifier for this form
-    validate,
-  })(NewProjectDialog)
-);
+)(NewProjectDialog);
---
+++ src/js/components/team-projects-view/new-project-form.tsx
@@ -0,0 +1,67 @@
+import * as React from 'react';
+import { Field, BaseFieldProps, FormProps, reduxForm } from 'redux-form';
+
+const styles = require('../common/modal-dialog.scss');
+
+interface PassedProps {
+  existingProjectNames: string[];
+}
+
+interface FormData {
+  name?: string;
+  description?: string;
+}
+
+type Props = PassedProps & FormProps<FormData, any>;
+
+const validate = (values: FormData, props: Props) => {
+  const errors: FormData = {};
+  const projectNameRegex = /^[a-z0-9\-]+$/;
+
+  const { name, description } = values;
+
+  if (!name) {
+    errors.name = 'Required';
+  } else if (!projectNameRegex.test(name)) {
+    errors.name = 'Only lower-case letters, numbers and dashes allowed.';
+  } else if (props.existingProjectNames.indexOf(name) > -1) {
+    errors.name = 'Project name already exists';
+  }
+
+  return errors;
+};
+
+const RenderField = ({ input, name, label, placeholder, type, meta: { touched, error }}: BaseFieldProps) => (
+  <div className="row">
+    <div className="col-xs-4">
+      <label htmlFor={name}>{label}</label>
+    </div>
+    <div className="col-xs-8">
+      <input {...input} placeholder={placeholder} type={type} />
+      {touched && error && <span className={styles.error}>{error}</span>}
+    </div>
+  </div>
+);
+
+class NewProjectForm extends React.Component<Props, any> {
+  public render() {
+    const { handleSubmit, pristine, submitting } = this.props;
+
+    return (
+      <form onSubmit={handleSubmit}>
+        <Field name="name" component={RenderField} type="text" label="Name" placeholder="my-project-name" />
+        <Field name="description" component={RenderField} type="textarea" label="Description" placeholder="Describe your project" />
+        <div className="row">
+          <div className="col-xs-12">
+            <button type="submit" disabled={pristine || submitting}>Create project</button>
+          </div>
+        </div>
+      </form>
+    );
+  }
+};
+
+export default reduxForm({
+  form: 'newProject',
+  validate,
+})(NewProjectForm);
