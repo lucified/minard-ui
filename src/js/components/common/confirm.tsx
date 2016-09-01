6107477ac9f72aa14573f0ca2d9ee0fdf6386336
Ville Saarinen <ville.saarinen@gmail.com>
2016-09-01 17:11:47 +0300

    Add Confirm component

---
+++ src/js/components/common/confirm.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import * as ModalDialog from 'react-modal';
+
+const ReactConfirm = require('react-confirm');
+
+const styles = require('./confirm.scss');
+
+interface Props {
+  show: boolean; // from confirmable. indicates if the dialog is shown or not.
+  proceed: (args?: any) => void; // from confirmable. call to close the dialog with promise resolved.
+  cancel: (args?: any) => void; // from confirmable. call to close the dialog with promise rejected.
+  dismiss: (args?: any) => void; // from confirmable. call to only close the dialog.
+  confirmation: string; // confirmation message from the confirm function
+  options: any; // options from the confirm function
+}
+
+let ConfirmDialog = ({show, proceed, dismiss, cancel, confirmation, options}: Props) => (
+  <ModalDialog className={styles.dialog} overlayClassName={styles.overlay} onRequestClose={dismiss} isOpen={show}>
+    <div>{confirmation}</div>
+    <div>
+      <button onClick={() => cancel()}>Cancel</button>
+      <button onClick={() => proceed()}>OK</button>
+    </div>
+  </ModalDialog>
+);
+
+ConfirmDialog = ReactConfirm.confirmable(ConfirmDialog);
+const confirmFunction = ReactConfirm.createConfirmation(ConfirmDialog);
+
+export default function(confirmation: string, options = {}) {
+  // You can pass whatever you want to the component. These arguments will be your Component's props
+  return confirmFunction({ confirmation, options });
+}
