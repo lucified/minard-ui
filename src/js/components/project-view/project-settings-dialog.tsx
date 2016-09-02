14255813505bd6de9d8ab05bfcf93dba67585d20
Ville Saarinen <ville.saarinen@gmail.com>
2016-09-02 12:50:59 +0300

    Add deletion to project settings dialog

--- src/js/components/project-view/project-settings-dialog.tsx
+++ src/js/components/project-view/project-settings-dialog.tsx
@@ -2,11 +2,13 @@ import * as React from 'react';
 import * as Icon from 'react-fontawesome';
 import * as ModalDialog from 'react-modal';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
 import { Dispatch } from 'redux';
 
-import { isFetchError } from '../../modules/errors';
+import Errors, { DeleteError, isFetchError } from '../../modules/errors';
 import Modal, { ModalType } from '../../modules/modal';
 import Projects, { Project } from '../../modules/projects';
+import Requests from '../../modules/requests';
 import { StateTree } from '../../reducers';
 
 import confirm from '../common/confirm';
@@ -18,13 +20,21 @@ interface PassedProps {
   project: Project;
 }
 
+interface InjectedProps {
+  router: ReactRouter.RouterOnContext;
+}
+
 interface GeneratedStateProps {
   isOpen: boolean;
   existingProjectNames: string[];
+  deletionInProgress: boolean;
+  deletionError?: DeleteError;
 }
 
 interface GeneratedDispatchProps {
   closeDialog: () => void;
+  deleteProject: (id: string, resolve: () => void, reject: () => void) => void;
+  clearDeletionErrors: () => void;
 }
 
 interface FormData {
@@ -32,52 +42,93 @@ interface FormData {
   description?: string;
 }
 
-type Props = PassedProps & GeneratedStateProps & GeneratedDispatchProps;
+type Props = PassedProps & GeneratedStateProps & GeneratedDispatchProps & InjectedProps;
 
-const confirmDeletion = (project: Project) => {
-  return (e: any) => {
+class ProjectSettingsDialog extends React.Component<Props, any> {
+  constructor(props: Props) {
+    super(props);
+
+    this.confirmDeletion = this.confirmDeletion.bind(this);
+    this.clearAndClose = this.clearAndClose.bind(this);
+  }
+
+  private confirmDeletion(e: any) {
     e.preventDefault();
+
+    const { project, deleteProject, closeDialog, router } = this.props;
+
     confirm(`Are you sure you want to delete ${project.name}? This action can't be undone`)
-      .then(() => console.log('will delete'))
+      .then(() => new Promise((resolve, reject) => {
+          deleteProject(project.id, resolve, reject);
+        })
+      )
+      .then(() => {
+        closeDialog();
+        router.push('/projects');
+      })
       .catch(() => {}); // tslint:disable-line
+  };
+
+  private clearAndClose() {
+    this.props.clearDeletionErrors();
+    this.props.closeDialog();
+  }
+
+  public render() {
+    const { project, isOpen, deletionInProgress, deletionError, existingProjectNames } = this.props;
+
+    return (
+      <ModalDialog
+        isOpen={isOpen}
+        onRequestClose={this.clearAndClose}
+        className={styles.dialog}
+        overlayClassName={styles.overlay}
+      >
+        <div onClick={this.clearAndClose} className={styles.close}>
+          <Icon name="times-circle" size="4x" />
+        </div>
+        <h1>Project settings</h1>
+        <ProjectSettingsForm
+          existingProjectNames={existingProjectNames}
+          onSubmitSuccess={this.clearAndClose}
+          initialValues={project}
+        />
+        {deletionInProgress ? (
+          <span className={styles.deleting}>Deleting...</span>
+        ) : (
+          <div className={styles.delete}>
+            <a onClick={this.confirmDeletion}>
+              Delete project
+            </a>
+            {deletionError && <div className={styles.error}>{deletionError.prettyError}</div>}
+          </div>
+        )}
+      </ModalDialog>
+    );
+  }
+}
+
+const mapStateToProps = (state: StateTree, ownProps: PassedProps): GeneratedStateProps => {
+  const { project } = ownProps;
+
+  return {
+    isOpen: Modal.selectors.isModalOpenOfType(state, ModalType.ProjectSettings),
+    existingProjectNames: Projects.selectors.getProjects(state)
+      .filter(projectOrError => !isFetchError(projectOrError))
+      .filter(otherProject => (otherProject as Project).name !== project.name) // filter out own name
+      .map(otherProject => (otherProject as Project).name),
+    deletionInProgress: Requests.selectors.isDeletingProject(state, project.id),
+    deletionError: Errors.selectors.getProjectDeletionError(state, project.id),
   };
 };
 
-const ProjectSettingsDialog = ({ project, isOpen, closeDialog, existingProjectNames }: Props) => (
-  <ModalDialog
-    isOpen={isOpen}
-    onRequestClose={closeDialog}
-    className={styles.dialog}
-    overlayClassName={styles.overlay}
-  >
-    <div onClick={closeDialog} className={styles.close}>
-      <Icon name="times-circle" size="4x" />
-    </div>
-    <h1>Project settings</h1>
-    <ProjectSettingsForm
-      existingProjectNames={existingProjectNames}
-      onSubmitSuccess={closeDialog}
-      initialValues={project}
-    />
-    <a className={styles.delete} onClick={confirmDeletion(project)}>
-      Delete project
-    </a>
-  </ModalDialog>
-);
-
-const mapStateToProps = (state: StateTree, ownProps: PassedProps) => ({
-  isOpen: Modal.selectors.isModalOpenOfType(state, ModalType.ProjectSettings),
-  existingProjectNames: Projects.selectors.getProjects(state)
-    .filter(projectOrError => !isFetchError(projectOrError))
-    .filter(project => (project as Project).name !== ownProps.project.name) // filter out own name
-    .map(project => (project as Project).name),
-});
-
-const mapDispatchToProps = (dispatch: Dispatch<any>) => ({
+const mapDispatchToProps = (dispatch: Dispatch<any>): GeneratedDispatchProps => ({
   closeDialog: () => dispatch(Modal.actions.closeModal(ModalType.ProjectSettings)),
+  deleteProject: (id, resolve, reject) => dispatch(Projects.actions.deleteProjectPromiseResolver(id, resolve, reject)),
+  clearDeletionErrors: () => dispatch(Errors.actions.clearProjectDeletionErrors()),
 });
 
 export default connect<GeneratedStateProps, GeneratedDispatchProps, PassedProps>(
   mapStateToProps,
   mapDispatchToProps,
-)(ProjectSettingsDialog);
+)(withRouter(ProjectSettingsDialog));
