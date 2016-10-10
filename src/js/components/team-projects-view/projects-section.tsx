ae93e2645043f0997d4f211f632a77eab5a3e925
Ville Saarinen <ville.saarinen@gmail.com>
2016-10-10 13:48:35 +0300

    Add transitions to projects in Team Projects View

--- src/js/components/team-projects-view/projects-section.tsx
+++ src/js/components/team-projects-view/projects-section.tsx
@@ -1,5 +1,6 @@
 import * as classNames from 'classnames';
 import * as React from 'react';
+import * as FlipMove from 'react-flip-move';
 import { connect } from 'react-redux';
 import { Dispatch } from 'redux';
 
@@ -32,56 +33,59 @@ interface GeneratedDispatchProps {
 
 type Props = PassedProps & GeneratedDispatchProps & GeneratedStateProps;
 
-const ProjectsSection = ({ projects, isLoading, openCreateNewProjectDialog, showAll, count = 6 }: Props) => {
-  const filteredProjects = (projects.filter(project => !isFetchError(project)) as Project[])
-    .sort((a, b) => b.latestActivityTimestamp - a.latestActivityTimestamp);
-  const projectsToShow = showAll ? filteredProjects : filteredProjects.slice(0, count);
-  // If we're only showing some of the projects, don't show the loading indicator if we have enough to show
-  const showLoadingIcon = isLoading && (showAll || (filteredProjects.length < count));
+class ProjectsSection extends React.Component<Props, any> {
+  public render() {
+    const { projects, isLoading, openCreateNewProjectDialog, showAll, count = 6 } = this.props;
+    const filteredProjects = (projects.filter(project => !isFetchError(project)) as Project[])
+      .sort((a, b) => b.latestActivityTimestamp - a.latestActivityTimestamp);
+    const projectsToShow = showAll ? filteredProjects : filteredProjects.slice(0, count);
+    // If we're only showing some of the projects, don't show the loading indicator if we have enough to show
+    const showLoadingIcon = isLoading && (showAll || (filteredProjects.length < count));
 
-  return (
-    <section className="container">
-      <NewProjectDialog />
-      <SectionTitle
-        rightContent={(
-          <a onClick={openCreateNewProjectDialog} className={classNames(styles['add-project-link'])}>
-            + Add new project
-          </a>
+    return (
+      <section className="container">
+        <NewProjectDialog />
+        <SectionTitle
+          rightContent={(
+            <a onClick={openCreateNewProjectDialog} className={classNames(styles['add-project-link'])}>
+              + Add new project
+            </a>
+          )}
+        >
+          <span>
+            {showAll ? 'All' : 'Latest'} projects for <span className={styles.team}>Team Lucify</span>
+          </span>
+        </SectionTitle>
+        <FlipMove className="row center-xs start-sm" enterAnimation="elevator" leaveAnimation="elevator">
+          {projectsToShow.map(project => (
+            <div key={project.id} className={classNames('col-xs-12', 'col-sm-6', 'col-md-4', styles['project-card'])}>
+              <ProjectCard project={project} />
+            </div>
+          ))}
+          {showLoadingIcon && (
+            <div key="loading" className={classNames('col-xs-12', 'col-sm-6', 'col-md-4', styles['project-card'])}>
+              <LoadingIcon className={styles.loading} center />
+            </div>
+          )}
+          {!showLoadingIcon && projectsToShow.length === 0 && (
+            <div key="initial" className={classNames('col-xs-12', styles.empty)}>
+              <h2>Create a new project to get started</h2>
+            </div>
+          )}
+        </FlipMove>
+        {(!showAll && filteredProjects.length > count) && (
+          <div className="row end-xs">
+            <div className={classNames('col-xs-12', styles['show-all-projects-section'])}>
+              <MinardLink className={styles['show-all-projects-link']} showAll homepage>
+                Show all projects ({filteredProjects.length})
+              </MinardLink>
+            </div>
+          </div>
         )}
-      >
-        <span>
-          {showAll ? 'All' : 'Latest'} projects for <span className={styles.team}>Team Lucify</span>
-        </span>
-      </SectionTitle>
-      <div className="row center-xs start-sm">
-        {projectsToShow.map(project => (
-          <div key={project.id} className={classNames('col-xs-12', 'col-sm-6', 'col-md-4', styles['project-card'])}>
-            <ProjectCard project={project} />
-          </div>
-        ))}
-        {showLoadingIcon && (
-          <div className={classNames('col-xs-12', 'col-sm-6', 'col-md-4', styles['project-card'])}>
-            <LoadingIcon className={styles.loading} center />
-          </div>
-        )}
-        {!showLoadingIcon && projectsToShow.length === 0 && (
-          <div className={classNames('col-xs-12', styles.empty)}>
-            <h2>Create a new project to get started</h2>
-          </div>
-        )}
-      </div>
-      {(!showAll && filteredProjects.length > count) && (
-        <div className="row end-xs">
-          <div className={classNames('col-xs-12', styles['show-all-projects-section'])}>
-            <MinardLink className={styles['show-all-projects-link']} showAll homepage>
-              Show all projects ({filteredProjects.length})
-            </MinardLink>
-          </div>
-        </div>
-      )}
-    </section>
-  );
-};
+      </section>
+    );
+  }
+}
 
 const mapDispatchToProps = (dispatch: Dispatch<any>): GeneratedDispatchProps => ({
   openCreateNewProjectDialog: (e: any) => {
