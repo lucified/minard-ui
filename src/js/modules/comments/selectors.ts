1a6ce416e58383016857e6f7b289ab6d0f8dedc6
Ville Saarinen <ville.saarinen@gmail.com>
2016-12-05 18:34:19 +0200

    Remove unused selector

--- src/js/modules/comments/selectors.ts
+++ src/js/modules/comments/selectors.ts
@@ -1,7 +1,5 @@
-import { values } from 'lodash';
-
 import { StateTree } from '../../reducers';
-import { FetchError, isFetchError } from '../errors';
+import { FetchError } from '../errors';
 
 import * as t from './types';
 
@@ -9,7 +7,3 @@ const selectCommentTree = (state: StateTree): t.CommentState => state.entities.c
 
 export const getComment = (state: StateTree, id: string): t.Comment | FetchError | undefined =>
   selectCommentTree(state)[id];
-export const getCommentsForDeployment = (state: StateTree, id: string): t.Comment[] =>
-  values<t.Comment>(selectCommentTree(state)).filter(
-    commentOrError => !isFetchError(commentOrError) && commentOrError.deployment === id,
-  );
