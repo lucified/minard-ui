6bab8d9259100202d6ad67b4ef1db6308b26522f
Ville Saarinen <ville.saarinen@gmail.com>
2016-12-07 21:05:04 +0200

    Add missing requests in Requests reducer

--- src/js/modules/requests/reducer.ts
+++ src/js/modules/requests/reducer.ts
@@ -16,10 +16,11 @@ const returnFilteredStateIfChanged = (
 const reducer: Reducer<t.RequestsState> = (state = initialState, action: any) => {
   switch (action.type) {
     case actions.Projects.LoadAllProjects.REQUEST.type:
-    case actions.Activities.LoadAllActivities.REQUEST.type:
     case actions.Projects.DeleteProject.REQUEST.type:
+    case actions.Activities.LoadAllActivities.REQUEST.type:
     case actions.Activities.LoadActivitiesForProject.REQUEST.type:
     case actions.Commits.LoadCommitsForBranch.REQUEST.type:
+    case actions.Comments.LoadCommentsForDeployment.REQUEST.type:
     case actions.ALL_ACTIVITIES_REQUESTED:
     case actions.ALL_ACTIVITIES_REQUESTED_FOR_PROJECT:
       return state.concat(action);
@@ -29,6 +30,19 @@ const reducer: Reducer<t.RequestsState> = (state = initialState, action: any) =>
       return returnFilteredStateIfChanged(
         state,
         requestInfo => (requestInfo.type !== actions.Activities.LoadActivitiesForProject.REQUEST.type) ||
+          (requestInfo.id !== action.id),
+      );
+    case actions.Activities.LoadAllActivities.FAILURE.type:
+    case actions.Activities.LoadAllActivities.SUCCESS.type:
+      return returnFilteredStateIfChanged(
+        state,
+        requestInfo => requestInfo.type !== actions.Activities.LoadAllActivities.REQUEST.type,
+      );
+    case actions.Comments.LoadCommentsForDeployment.FAILURE.type:
+    case actions.Comments.LoadCommentsForDeployment.SUCCESS.type:
+      return returnFilteredStateIfChanged(
+        state,
+        requestInfo => (requestInfo.type !== actions.Comments.LoadCommentsForDeployment.REQUEST.type) ||
           (requestInfo.id !== action.id),
       );
     case actions.Commits.LoadCommitsForBranch.FAILURE.type:
@@ -43,12 +57,6 @@ const reducer: Reducer<t.RequestsState> = (state = initialState, action: any) =>
       return returnFilteredStateIfChanged(
         state,
         requestInfo => requestInfo.type !== actions.Projects.LoadAllProjects.REQUEST.type,
-      );
-    case actions.Activities.LoadAllActivities.FAILURE.type:
-    case actions.Activities.LoadAllActivities.SUCCESS.type:
-      return returnFilteredStateIfChanged(
-        state,
-        requestInfo => requestInfo.type !== actions.Activities.LoadAllActivities.REQUEST.type,
       );
     case actions.Projects.DeleteProject.SUCCESS.type:
     case actions.Projects.DeleteProject.FAILURE.type:
