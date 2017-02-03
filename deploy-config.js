b35862899da55c3b5d4618ae1007ff8dbf7ec010
Ville Saarinen <ville.saarinen@gmail.com>
2017-02-03 10:29:58 +0200

    Take 3

--- deploy-config.js
+++ deploy-config.js
@@ -14,11 +14,6 @@ const opts = {
     return 'https://staging.minard.io/';
   },
   publicPath: (env) => {
-    // For Minard
-    if (process.env.GITLAB_CI) {
-      return '/';
-    }
-
     if (['production', 'staging'].indexOf(env) > -1) {
       return '/';
     }
@@ -28,8 +23,14 @@ const opts = {
   flow: 'bdc6c13b-be3f-42a9-9f71-e9197dd8fb03', // The Main flow ID
 };
 
+// LUCIFY_ENV is used by chatops. We always want to follow it if it's been set.
+// CIRCLE_BRANCH is used by Circle. We automatically want to deploy the master
+// branch to staging.
+// GITLAB_CI has been set if we're in Minard. We want to simulate a staging
+// environment in Minard.
+// Otherwise, we use NODE_ENV or fall back to 'development'
 const env = process.env.LUCIFY_ENV || (
-  process.env.CIRCLE_BRANCH === 'master' ?
+  (process.env.CIRCLE_BRANCH === 'master' || process.env.GITLAB_CI) ?
     'staging' :
     process.env.NODE_ENV || 'development'
 );
