55d3c9f4dd433619186552656860c70afa46df30
Ville Saarinen <ville.saarinen@gmail.com>
2017-04-04 15:53:45 +0300

    Re-add team information loading to Projects Frame
    This is needed if the user enters the app without going through the Login View.

--- src/js/components/projects-frame.tsx
+++ src/js/components/projects-frame.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { connect, Dispatch } from 'react-redux';
 
 import Projects from '../modules/projects';
+import Requests from '../modules/requests';
 import User, { Team } from '../modules/user';
 import { StateTree } from '../reducers';
 
@@ -17,11 +18,13 @@ interface PassedProps {
 
 interface GeneratedDispatchProps {
   loadAllProjects: (teamId: string) => void;
+  loadTeamInformation: () => void;
   redirectToLogin: () => void;
 }
 
 interface GeneratedStateProps {
   isUserLoggedIn: boolean;
+  isLoadingTeamInformation: boolean;
   team?: Team;
 }
 
@@ -29,19 +32,40 @@ type Props = GeneratedDispatchProps & PassedProps & GeneratedStateProps;
 
 class ProjectsFrame extends React.Component<Props, void> {
   public componentWillMount() {
-    const { loadAllProjects, isUserLoggedIn, redirectToLogin, team } = this.props;
+    const { loadAllProjects, isUserLoggedIn, redirectToLogin, team, loadTeamInformation } = this.props;
 
     if (!isUserLoggedIn) {
       redirectToLogin();
-    } else if (team !== undefined) {
+    } else if (team === undefined) {
+      // TODO: team information is now being fetched in three places:
+      // - Login View after getting user login information from Auth0
+      // - Signup View after getting user information (using the backend's signup endpoint)
+      // - Here
+      //
+      // We should probably try to reduce the number of places.
+      loadTeamInformation();
+    } else {
       loadAllProjects(team.id);
     }
   }
 
+  public componentWillReceiveProps(nextProps: Props) {
+    const { loadAllProjects, team } = this.props;
+
+    if (nextProps.team && team === undefined) {
+      loadAllProjects(nextProps.team.id);
+    }
+  }
+
   public render() {
-    const { children, team } = this.props;
+    const { children, team, isLoadingTeamInformation } = this.props;
 
     if (!team) {
+      if (isLoadingTeamInformation) {
+        // TODO: better loading indicator
+        return <div>Loading...</div>;
+      }
+
       // TODO: better error indicator
       return <div>Unable to load team information</div>;
     }
@@ -60,10 +84,12 @@ class ProjectsFrame extends React.Component<Props, void> {
 const mapDispatchToProps = (dispatch: Dispatch<any>): GeneratedDispatchProps => ({
   loadAllProjects: (teamId: string) => { dispatch(Projects.actions.loadAllProjects(teamId)); },
   redirectToLogin: () => { dispatch(User.actions.redirectToLogin()); },
+  loadTeamInformation: () => { dispatch(User.actions.loadTeamInformation()); },
 });
 
 const mapStateToProps = (state: StateTree): GeneratedStateProps => ({
   isUserLoggedIn: User.selectors.isUserLoggedIn(state),
+  isLoadingTeamInformation: Requests.selectors.isLoadingTeamInformation(state),
   team: User.selectors.getTeam(state),
 });
 
