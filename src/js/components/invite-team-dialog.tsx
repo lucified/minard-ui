844b3778a52f557df05a9e66f66aab9e616ba60e
Ville Saarinen <ville.saarinen@gmail.com>
2017-04-27 14:12:41 +0300

    Style dialog content

--- src/js/components/invite-team-dialog.tsx
+++ src/js/components/invite-team-dialog.tsx
@@ -1,3 +1,4 @@
+import * as classNames from 'classnames';
 import * as React from 'react';
 import Icon = require('react-fontawesome');
 import * as ModalDialog from 'react-modal';
@@ -7,7 +8,8 @@ import { Dispatch } from 'redux';
 import Modal, { ModalType } from '../modules/modal';
 import { StateTree } from '../reducers';
 
-const styles = require('./common/forms/modal-dialog.scss');
+const modalStyles = require('./common/forms/modal-dialog.scss');
+const styles = require('./invite-team-dialog.scss');
 
 interface PassedProps {
   invitationToken?: string;
@@ -27,6 +29,22 @@ function getParentElement() {
   return document.querySelector('#minard-app') as HTMLElement;
 }
 
+function selectText(e: React.MouseEvent<HTMLElement>) {
+  const node = e.target;
+  const doc = document as any;
+
+  if (doc.selection) {
+    const range = doc.body.createTextRange();
+    range.moveToElementText(node);
+    range.select();
+  } else if (window.getSelection) {
+    const range = document.createRange();
+    range.selectNodeContents(node as Node);
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(range);
+  }
+}
+
 class NewProjectDialog extends React.Component<Props, void> {
   public render() {
     const { isOpen, closeDialog, invitationToken } = this.props;
@@ -36,31 +54,33 @@ class NewProjectDialog extends React.Component<Props, void> {
         isOpen={isOpen}
         onRequestClose={closeDialog}
         closeTimeoutMS={150}
-        className={styles.dialog}
-        overlayClassName={styles.overlay}
+        className={modalStyles.dialog}
+        overlayClassName={modalStyles.overlay}
         parentSelector={getParentElement}
         contentLabel="Invite team members"
       >
-        <header className={styles.header}>
+        <header className={modalStyles.header}>
           <div>Invite your team</div>
-          <div onClick={closeDialog} className={styles.close}>
+          <div onClick={closeDialog} className={modalStyles.close}>
             <Icon name="times" />
           </div>
         </header>
-        <p>
-          Get your team onboard by sharing the following signup URL with them:
-        </p>
-        <pre>
-          {
-            `${window.location.protocol}//${window.location.hostname}` +
-              (window.location.port !== '80' ? `:${window.location.port}` : '') +
-              `/signup/${invitationToken}`
-          }
-        </pre>
-        <p>
-          <strong>Be careful where you share this URL! </strong>
-          Anyone can join your team using it.
-        </p>
+        <div className={classNames(modalStyles.form, styles.instructions)}>
+          <p>Get your team onboard by sharing this signup URL with them:</p>
+          <div className={styles.url}>
+            <pre onClick={selectText}>
+              {
+                `${window.location.protocol}//${window.location.hostname}` +
+                  (window.location.port !== '80' ? `:${window.location.port}` : '') +
+                  `/signup/${invitationToken}`
+              }
+            </pre>
+          </div>
+          <p>
+            <strong>Be mindful of where you share this URL!</strong><br />
+            Anyone can join your team with it.
+          </p>
+        </div>
       </ModalDialog>
     );
   }
