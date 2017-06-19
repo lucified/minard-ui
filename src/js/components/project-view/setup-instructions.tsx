58a582cac375bb254cd70d55b1509727685d1720
Juho Ojala <juho@lucify.com>
2017-06-19 15:19:11 +0300

    Fix copy in setup-instructions

--- src/js/components/project-view/setup-instructions.tsx
+++ src/js/components/project-view/setup-instructions.tsx
@@ -42,7 +42,7 @@ const SetupInstructions = ({ project }: Props) => {
             <div className={styles.text}>
               …Or, if you already use GitHub or another remote repository in
               your project, add Minard
-              as a new remote and secodary URL to origin with:
+              as a new remote and a secondary URL to origin with:
             </div>
             <div className={styles.code}>
               <pre>
